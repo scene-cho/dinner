54bd15cc0a59e81ba8f3ed7c12f82b4094e070f6
scene-cho <scene_cho@naver.com>
2020-09-04 19:04:52 +0900

    [#1] Bootstrap

---
+++ src/main/resources/static/js/sidebar.js
@@ -0,0 +1,9 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("sidebar").style.width = "100%";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("sidebar").style.width = "0";
+}
